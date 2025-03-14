ebb5de32e1f4e66a23c1d462825b1dbcbfea0f81
Ugur Cetin <gorunmezadam.usa@gmail.com>
2025-03-14 18:01:40 -0500

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,119 @@
+console.log("Step 1 working");
+
+// Create the 'basemap' tile layer that will be the background of our map.
+let basemap = L.tileLayer(
+  "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'",
+  {
+    attribution:
+      'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+  });
+
+
+// Create the map object with center and zoom options.
+let map = L.map("map", {
+  center: [
+    40.7, -94.5
+  ],
+  zoom: 3
+});
+
+// Then add the 'basemap' tile layer to the map.
+basemap.addTo(map);
+
+// Make a request that retrieves the earthquake geoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function (data) {
+
+  // This function returns the style data for each of the earthquakes we plot on
+  // the map. Pass the magnitude and depth of the earthquake into two separate functions
+  // to calculate the color and radius.
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: getColor(feature.geometry.coordinates[2]),
+      color: "#000000",
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+    };
+  }
+
+  // This function determines the color of the marker based on the depth of the earthquake.
+  function getColor(depth) {
+    if (depth > 90) {
+      return "#ea2c2c";
+    } else if (depth > 70) {
+      return "#ea822c";
+    } else if (depth > 50) {
+      return "#ee9c00";
+    } else if (depth > 30) {
+      return "#eecc00";
+    } else if (depth > 10) {
+      return "#d4ee00";
+    } else {
+      return "#98ee00";
+    }
+  }
+
+  // This function determines the radius of the earthquake marker based on its magnitude.
+  // Earthquakes with a magnitude of 0 were being plotted with the wrong radius.
+  function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+
+    return magnitude * 4;
+  }
+
+  // Add a GeoJSON layer to the map once the file is loaded.
+  L.geoJson(data, {
+    // Turn each feature into a circleMarker on the map.
+    pointToLayer: function (feature, latlng) {
+      return L.circleMarker(latlng);
+    },
+    // Set the style for each circleMarker using our styleInfo function.
+    style: styleInfo,
+    // Create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
+    onEachFeature: function (feature, layer) {
+      layer.bindPopup(
+        "Magnitude: "
+        + feature.properties.mag
+        + "<br>Depth: "
+        + feature.geometry.coordinates[2]
+        + "<br>Location: "
+        + feature.properties.place
+      );
+    }
+  }).addTo(map);
+
+  // Create a legend control object.
+  let legend = L.control({
+    position: "bottomright"
+  });
+
+  // Then add all the details for the legend
+  legend.onAdd = function () {
+    let div = L.DomUtil.create("div", "info legend");
+
+    // Initialize depth intervals and colors for the legend
+    let grades = [-10, 10, 30, 50, 70, 90];
+    let colors = [
+      "#98ee00",
+      "#d4ee00",
+      "#eecc00",
+      "#ee9c00",
+      "#ea822c",
+      "#ea2c2c"
+    ];
+
+    // Loop through our depth intervals to generate a label with a colored square for each interval.
+    for (let i = 0; i < grades.length; i++) {
+      div.innerHTML += "<i style='background: " + colors[i] + "'></i> "
+        + grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+    }
+    return div;
+  };
+
+  // Finally, add the legend to the map.
+  legend.addTo(map);
+});
